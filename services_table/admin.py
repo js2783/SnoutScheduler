ba745543388e3f955f2e329a59fd7cb407bf0b6f
iammjackiee <jgutierrez80@ivytech.edu>
2025-10-08 17:41:16 -0500

    Update admin.py

--- services_table/admin.py
+++ services_table/admin.py
@@ -4,3 +4,10 @@ from django.contrib import admin
 from .models import Post
 
 admin.site.register(Post)
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "duration_minutes", "formatted_price", "active")
+    list_filter = ("active", "category")
+    search_fields = ("name", "description")
+
