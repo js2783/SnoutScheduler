e8ef113d65e56787dcc810bf6a457d421dd9a11c
Aaron <astichter@ivytech.edu>
2025-10-17 10:55:27 -0400

    added more available timeslots

--- booking/api.py
+++ booking/api.py
@@ -17,5 +17,5 @@ class ApiClient:
                 {"id": 4, "name": "Jordan"},
                 {"id": 5, "name": "Taylor"},
                 ]
-    def list_time_slots(self,date): return ['10:00','11:00','12:00','13:00']
+    def list_time_slots(self,date): return ['8:00am','9:00am','10:00am','11:00am','12:00pm','1:00pm','2:00pm','3:00pm','4:00pm']
     def submit_appointment_request(self,payload): return {'ok':True,'ref':f"SCOUT-{random.randint(100000,999999)}"}
